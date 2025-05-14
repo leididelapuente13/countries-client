e75593addb2c86211097a6df45741941b1fbb257
Leidi <ldelapuente.sysnet@outlook.com>
2025-05-14 17:54:10 -0500

    (feat): layout component

---
+++ src/app/countries/layout/layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from "../../shared/components/layout/top-bar/top-bar.component";
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterOutlet, TopBarComponent],
+  templateUrl: './layout.component.html',
+})
+export class LayoutComponent { }
