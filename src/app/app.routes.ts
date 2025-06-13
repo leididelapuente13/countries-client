b286ddfdb63c5c3cff4f3a9dab02374d18dab293
Leidi De La Puente <leididelapuente3@gmail.com>
2025-06-13 15:18:45 -0500

    (refactor): remove country router and use the app router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,20 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './countries/layout/layout.component';
+import { CountriesPageComponent } from './countries/pages/countries-page/countries-page.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./countries/countries.routes'),
+    path: 'countries',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: CountriesPageComponent
+      }
+    ]
   },
+  {
+    path: '**',
+    redirectTo: 'countries'
+  }
 ];
