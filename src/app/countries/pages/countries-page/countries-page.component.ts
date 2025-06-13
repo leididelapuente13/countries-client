ed2a7367943a7ca4b5824902e69e567f09ac5b1a
Leidi De La Puente <leididelapuente3@gmail.com>
2025-06-13 15:14:46 -0500

    (feat):  add the search country by name resource

--- src/app/countries/pages/countries-page/countries-page.component.ts
+++ src/app/countries/pages/countries-page/countries-page.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
 import { ListComponent } from "../../components/list/list.component";
 
 import countriesData from '../../../shared/utils/data.json';
+import { CountriesService } from '../../services/countries.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-countries-page',
@@ -10,5 +12,15 @@ import countriesData from '../../../shared/utils/data.json';
   templateUrl: './countries-page.component.html',
 })
 export class CountriesPageComponent {
+  countryService = inject(CountriesService);
   countries = countriesData;
+  countryNameQuery = signal<string>('');
+
+  countryByNameResource = resource({
+    request: ()=>({query: this.countryNameQuery()}),
+    loader: async ({request})=>{
+      if(request.query === '') return []
+      return await firstValueFrom(this.countryService.getCountriesByName(request.query))
+    }
+  })
 }
