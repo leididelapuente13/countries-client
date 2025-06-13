7fc2ee4b7dfb705fc9cee0dd6c4396ca1b08dde8
Leidi De La Puente <leididelapuente3@gmail.com>
2025-06-13 15:16:55 -0500

    (update): update country interface to separate it from the resr country

--- src/app/countries/interfaces/country.interface.ts
+++ src/app/countries/interfaces/country.interface.ts
@@ -1,32 +1,23 @@
 export interface Country {
   name: string;
   nativeName: string;
-  capital?: string;
+  capital: string;
   subregion: string;
   region: string;
   population: number;
+
   topLevelDomain: string[];
-  area?: number;
-  currencies?: Currency[];
-  languages?: Language[];
-  borderCountries?: string[];
+  area: number;
+  currency: string;
+  languages: string;
+  borderCountries: string[];
   flag: string;
 }
 
-interface Currency {
-  code: string;
-  name: string;
-  symbol: string;
-}
-
-interface Language {
-  iso639_1?: string;
-  iso639_2?: string;
-  name?: string;
-  nativeName?: string;
-}
-
-interface Flag {
-  svg: string;
-  png: string;
-}
+export type Region =
+    | 'Africa'
+    | 'Americas'
+    | 'Asia'
+    | 'Europe'
+    | 'Oceania'
+    | 'Antarctic';
