5fc4860785a5f8193ba6390b04e61c941995b7e1
Leidi De La Puente <leididelapuente3@gmail.com>
2025-06-13 15:15:13 -0500

    (feat): create rest country to country mapper

---
+++ src/app/countries/mappers/countries.mapper.ts
@@ -0,0 +1,33 @@
+import { RESTCountry } from "../interfaces/rest-countries.interface";
+import { Country } from '../interfaces/country.interface';
+
+export class CountryMapper {
+  static restCountrytoCountry(restCountry: RESTCountry): Country {
+    const { name, area, capital, subregion, region, population, currencies, languages, borders, flags, tld } = restCountry;
+
+    const currency: string = currencies ? Object.values(currencies)[0]?.name || '' : '';
+
+    const language: string = languages ? Object.values(languages)[0] : ''
+
+    const nativeName : string = name.nativeName ? Object.values(name.nativeName)[0]?.official : '';
+
+    return {
+      name: name.common,
+      capital: capital[0],
+      languages: language,
+      borderCountries: borders,
+      topLevelDomain: tld,
+      flag: flags.svg,
+      currency,
+      nativeName,
+      subregion,
+      region,
+      population,
+      area
+    }
+  }
+
+  static restCountriesToCountries(restCountries: RESTCountry[]): Country[] {
+    return restCountries.map(restCountry => this.restCountrytoCountry(restCountry));
+  }
+}
