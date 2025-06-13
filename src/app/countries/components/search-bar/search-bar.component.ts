be2e0a02bab9f606286c42e380c3b522a0bed3a8
Leidi De La Puente <leididelapuente3@gmail.com>
2025-06-13 16:07:22 -0500

    (feat): make the serach bar inputs emit the value of what will be search

--- src/app/countries/components/search-bar/search-bar.component.ts
+++ src/app/countries/components/search-bar/search-bar.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, effect, output, signal } from '@angular/core';
 
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { heroMagnifyingGlassSolid } from '@ng-icons/heroicons/solid';
+
+import { Region } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'country-search-bar',
   imports: [NgIcon],
   templateUrl: './search-bar.component.html',
   viewProviders: provideIcons({ heroMagnifyingGlassSolid }),
-
 })
-export class SearchBarComponent { }
+export class SearchBarComponent {
+  regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Antarctic',
+    'Asia',
+    'Europe',
+    'Oceania'
+  ]
+  countryToSearchByName = output<string>();
+  countryToSearchByRegion = output<Region>();
+
+  inputValue = signal<string>('');
+
+  debounceEffect = effect((onCleanUp) => {
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => {
+      this.countryToSearchByName.emit(value);
+    }, 500);
+
+    onCleanUp(() => {
+      clearTimeout(500)
+    })
+  });
+
+  onRegionChange(value: string) {
+    this.countryToSearchByName.emit('');
+    this.countryToSearchByRegion.emit(value as Region);
+  }
+}
