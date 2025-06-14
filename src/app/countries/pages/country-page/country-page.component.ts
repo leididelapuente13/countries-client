0e79e1d80b66b23b27e2a3e587dab6263d7ee6aa
Leidi De La Puente <leididelapuente3@gmail.com>
2025-06-13 23:52:38 -0500

    fix: refactor country page component to use signals for country code and include CountryButton component

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, resource } from '@angular/core';
+import { Component, inject, input, resource, signal } from '@angular/core';
 import { DecimalPipe, NgOptimizedImage } from '@angular/common'
 
 import { NgIcon, provideIcons } from '@ng-icons/core';
@@ -7,11 +7,13 @@ import { InformationComponent } from "../../components/card/information/informat
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { CountriesService } from '../../services/countries.service';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, map, Observable } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CountryButtonComponent } from "./country-button/country-button.component";
 
 @Component({
   selector: 'app-country-page',
-  imports: [NgOptimizedImage, DecimalPipe, NgIcon, InformationComponent],
+  imports: [NgOptimizedImage, DecimalPipe, NgIcon, InformationComponent, CountryButtonComponent],
   templateUrl: './country-page.component.html',
   viewProviders: provideIcons({ heroArrowLeftSolid }),
 })
@@ -19,13 +21,22 @@ export class CountryPageComponent {
 
   private route = inject(ActivatedRoute);
   private countriesService = inject(CountriesService);
-  private countryCode = this.route.snapshot.paramMap.get('code');
-
+  countryCode = toSignal(this.route.paramMap.pipe(
+    map(params => params.get('code'))
+  ));
   countryByCodeResource = resource({
     request: () => ({ code: this.countryCode }),
     loader: async ({ request }) => {
       if (!request.code) return;
-      return await firstValueFrom(this.countriesService.getCountryByCode(request.code));
+      return await firstValueFrom(this.countriesService.getCountryByCode(String(request.code)));
     },
+  });
+
+  countriesByCodeResource = resource({
+    request: () => ({ codes: this.countryByCodeResource.value()?.borderCountries }),
+    loader: async ({ request }) => {
+      if (!request.codes) return;
+      return await firstValueFrom(this.countriesService.getCountriesByCode(request.codes));
+    }
   })
 }
