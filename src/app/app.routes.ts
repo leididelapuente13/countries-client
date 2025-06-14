eb0ab53c39663ea1d32406b794cebf93a0adbbe4
Leidi De La Puente <leididelapuente3@gmail.com>
2025-06-13 22:22:00 -0500

    (feat): add route for country page component by code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './countries/layout/layout.component';
 import { CountriesPageComponent } from './countries/pages/countries-page/countries-page.component';
+import { CountryPageComponent } from './countries/pages/country-page/country-page.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,10 @@ export const routes: Routes = [
       {
         path: '',
         component: CountriesPageComponent
+      },
+      {
+        path: ':code',
+        component: CountryPageComponent
       }
     ]
   },
